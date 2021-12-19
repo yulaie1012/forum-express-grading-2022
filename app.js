b1e136e61b12b42c24d1abbb97600470ca66d7f7
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-19 22:11:04 +0800

    feat: initialize project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+require('./routes')(app)
+
+module.exports = app
