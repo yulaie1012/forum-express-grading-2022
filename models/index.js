a9b2a75917072103fbd2cbe8ab6d581202533c5e
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-19 22:22:21 +0800

    refactor: use javascript standard style

--- models/index.js
+++ models/index.js
@@ -1,11 +1,9 @@
-'use strict'
-
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config/config.json')[env]
+const config = require(path.join(__dirname, '/../config/config.json'))[env]
 const db = {}
 
 // 資料庫連線
@@ -23,7 +21,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file))
+    const model = sequelize.import(path.join(__dirname, file))
     db[model.name] = model
   })
 
