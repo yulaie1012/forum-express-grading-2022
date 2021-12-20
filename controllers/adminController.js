d3d5b5900047cf9cd9b0e4a06dbede232340a6ff
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-20 16:00:20 +0800

    fix: add missing try/catch to async/await

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,27 +3,35 @@ const { Restaurant } = db
 
 module.exports = {
   getRestaurants: async (req, res) => {
-    const restaurants = await Restaurant.findAll({ raw: true })
-    return res.render('admin/restaurants', { restaurants })
+    try {
+      const restaurants = await Restaurant.findAll({ raw: true })
+      return res.render('admin/restaurants', { restaurants })
+    } catch (err) {
+      console.error(err)
+    }
   },
 
-  createPage: async (req, res) => {
+  createPage: (req, res) => {
     return res.render('admin/create')
   },
 
   createRestaurant: async (req, res) => {
-    const { name, tel, address, openingHours, description } = req.body
+    try {
+      const { name, tel, address, openingHours, description } = req.body
 
-    if (!name) {
-      req.flash('errorMessage', '名字必須填寫！')
-      return res.redirect('back')
+      if (!name) {
+        req.flash('errorMessage', '名字必須填寫！')
+        return res.redirect('back')
+      }
+
+      await Restaurant.create({
+        name, tel, address, openingHours, description
+      })
+
+      req.flash('successMessage', '新增餐廳成功！')
+      return res.redirect('/admin/restaurants')
+    } catch (err) {
+      console.error(err)
     }
-
-    await Restaurant.create({
-      name, tel, address, openingHours, description
-    })
-
-    req.flash('successMessage', '新增餐廳成功！')
-    return res.redirect('/admin/restaurants')
   }
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,42 +9,46 @@ module.exports = {
   },
 
   signUp: async (req, res) => {
-    const { name, email, password, passwordCheck } = req.body
-    const errorMessages = []
+    try {
+      const { name, email, password, passwordCheck } = req.body
+      const errorMessages = []
 
-    if (!name || !email || !password || !passwordCheck) {
-      req.flash('errorMessage', '所有欄位都必須填寫！')
-      return res.redirect('/signup')
+      if (!name || !email || !password || !passwordCheck) {
+        req.flash('errorMessage', '所有欄位都必須填寫！')
+        return res.redirect('/signup')
+      }
+
+      if (password !== passwordCheck) {
+        errorMessages.push('兩次密碼輸入不同！')
+      }
+
+      const [checkName, checkEmail] = await Promise.all([
+        User.findOne({ where: { name } }),
+        User.findOne({ where: { email } })
+      ])
+
+      if (checkName) {
+        errorMessages.push('name 已經重複註冊！')
+      }
+
+      if (checkEmail) {
+        errorMessages.push('email 已經重複註冊！')
+      }
+
+      if (errorMessages.length) {
+        return res.render('signup', { errorMessages, name, email, password, passwordCheck })
+      }
+
+      await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+      })
+      req.flash('successMessage', '成功註冊此 email！')
+      return res.redirect('/signin')
+    } catch (err) {
+      console.error(err)
     }
-
-    if (password !== passwordCheck) {
-      errorMessages.push('兩次密碼輸入不同！')
-    }
-
-    const [checkName, checkEmail] = await Promise.all([
-      User.findOne({ where: { name } }),
-      User.findOne({ where: { email } })
-    ])
-
-    if (checkName) {
-      errorMessages.push('name 已經重複註冊！')
-    }
-
-    if (checkEmail) {
-      errorMessages.push('email 已經重複註冊！')
-    }
-
-    if (errorMessages.length) {
-      return res.render('signup', { errorMessages, name, email, password, passwordCheck })
-    }
-
-    await User.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-    })
-    req.flash('successMessage', '成功註冊此 email！')
-    return res.redirect('/signin')
   },
 
   signInPage: (req, res) => {
