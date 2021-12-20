feb04238462e91773f8b02fc000b13e478a2d596
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-20 15:07:00 +0800

    feat: add admin restaurant index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,9 @@
+const db = require('../models')
+const { Restaurant } = db
+
 module.exports = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: async (req, res) => {
+    const restaurants = await Restaurant.findAll({ raw: true })
+    return res.render('admin/restaurants', { restaurants })
   }
 }
